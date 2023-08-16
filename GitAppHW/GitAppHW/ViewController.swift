6764d4223551261f3d9e3b1861a3d4e2691fe5db
vaeryia <moyseychik.v@gmail.com>
2023-08-16 22:59:32 +0300

    Project created

---
+++ GitAppHW/GitAppHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitAppHW
+//
+//  Created by Valeria Moyseychik on 16.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
